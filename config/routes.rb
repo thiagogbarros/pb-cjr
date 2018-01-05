c0b2c1c1cfd70638d80ad7c076df693fe4e05f6a
Thiago <thiagobarros@cjr.org.br>
2018-01-04 23:35:41 -0200

    Gerando user com a devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :refunds
+  root to: "refunds#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
